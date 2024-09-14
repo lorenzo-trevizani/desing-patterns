bee415dad2f1b1b72eb26bb2a48155363d7bd0e7
Lorenzo Trevizani <lorenzotrevizani@Lorenzos-Laptop.local>
2024-09-14 18:06:33 -0300

    Uploading the custom module for magento

---
+++ registration.php
@@ -0,0 +1,15 @@
+<?php
+
+/**
+ * Certification Stamps' Product Attribute
+ *
+ * @package CustomVendor_CertificationStamps
+ * @author Lorenzo Trevizani <lm.trevizani@gmail.com>
+ * @license https://opensource.org/licenses/OSL-3.0.php Open Software License 3.0
+ */
+
+\Magento\Framework\Component\ComponentRegistrar::register(
+    \Magento\Framework\Component\ComponentRegistrar::MODULE,
+    'CustomVendor_CertificationStamps',
+    __DIR__
+);
